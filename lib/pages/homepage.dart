d3ec832e1ee861880d10bc404c049e7137885b93
aaryan.apte <aaryan3108@gmail.com>
2022-11-18 19:08:11 +0530

    This is 3rd update

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -12,20 +13,44 @@ class HomePage extends StatelessWidget {
         title: const Center(
           child: Center(
             child: Text("Shopping App",
-            style: TextStyle(fontSize: 20.0, fontFamily: "PlusJakartaSans-Regular"),),
+            style: TextStyle(fontSize: 20.0, fontFamily: "PlusJakartaSans-Regular", fontWeight: FontWeight.w800),
+            ),
           ),
         ),
         centerTitle: true,
         backgroundColor: Colors.blueGrey[900],
-        leading: BackButton(),
-        iconTheme: IconThemeData(color: Colors.blue[900],
+        leading: const BackButton(),
+        iconTheme: const IconThemeData(color: Colors.white,
         ),
         actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.search_rounded)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.shopping_cart)),
+          IconButton(onPressed: (){}, icon: const Icon(Icons.search_rounded)),
+          IconButton(onPressed: (){}, icon: const Icon(Icons.shopping_cart)),
         ],
       ),
-
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(30.0),
+            child: Text(
+              'Shop More! Udaao!!',
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400),
+            ),
+          ),
+          Expanded(
+            child: StaggeredGridView.countBuilder(crossAxisCount: 2,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.orange,
+                  );
+                },
+                staggeredTileBuilder: (index) => const StaggeredTile.fit(1)),
+          )
+        ],
+      ),
     );
   }
 }
